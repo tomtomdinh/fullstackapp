c6b95b5b927a33b14b298da140ca39e16c1a96c8
Tom <tomtomdinh@gmail.com>
2018-07-05 22:13:42 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// generates an application that runs the express modules
+// will listen to incoming requests and route them to handlers and send responses
+// underlying express server
+const app = express();
+
+// creates a route handler
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+// heroku can set underlying environment variables when it creates the application for production so it sets the port that heroku needs
+// so if there aren't any environment variables then this might not exist (heroku hasn't start up the app or we are working on dev)
+// we use 'or' to set to port 3000 if this environment variable doesn't exist
+const PORT = process.env.PORT || 3000;
+// tells the node runtime to listen to on to this port
+app.listen(PORT);
